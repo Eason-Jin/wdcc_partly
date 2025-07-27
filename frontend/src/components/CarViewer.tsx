5307ba6590a992c46a7af191913beef952f2808b
Leona Lu <leona_lu@qq.com>
2025-07-27 15:23:08 +1200

    add marker

--- frontend/src/components/CarViewer.tsx
+++ frontend/src/components/CarViewer.tsx
@@ -1,11 +1,42 @@
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useRef, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 import * as THREE from 'three';
+import { addInteractivePoint } from './addInteractivePoint';
 
 function CarModel() {
   const gltf = useGLTF('/carModel/scene.gltf') // 注意路径是相对 public 的
-  return <primitive object={gltf.scene} />
+  const sceneRef = useRef<THREE.Group>(null);
+  
+  // set the scale of the model
+  useEffect(() => {
+    if (gltf.scene) {
+      gltf.scene.scale.set(0.03, 0.03, 0.03) // 调整比例大小，试试不同的值
+    }
+  }, [gltf])
+
+
+  useEffect(() => {
+    if (sceneRef.current) {
+      // Add a point marker on the car
+      const rearCutPosition = new THREE.Vector3(15, 42, -80); // Adjust the position based on the model's scale and orientation
+      const rearCutMarker = addInteractivePoint('RearCutMarker', rearCutPosition, 'Rear Cut');
+      rearCutMarker.scale.set(20, 20, 20); 
+      sceneRef.current.add(rearCutMarker);
+
+      const tailboardPosition = new THREE.Vector3(15, 42, -90); // Adjust the position based on the model's scale and orientation
+      const tailboardMarker = addInteractivePoint('tailboardMarker', tailboardPosition, 'Tailboard Assembly');
+      rearCutMarker.scale.set(20, 20, 20); 
+      sceneRef.current.add(tailboardMarker);
+    }
+  }, []);
+
+
+
+ return <primitive ref={sceneRef} object={gltf.scene} />;
+
+
+
 }
 
 function RotatingLight() {
